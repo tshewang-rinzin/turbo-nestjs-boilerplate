74e8ee8081e559e793d29c47ad9ca2012196e6a5
Sonam Dema <sohana0808@gmail.com>
2022-05-29 21:07:17 +0600

    unique validator fix

--- src/validators/unique.validator.ts
+++ src/validators/unique.validator.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectConnection } from '@nestjs/typeorm';
 import type {
   ValidationArguments,
   ValidationOptions,
@@ -7,12 +6,14 @@ import type {
 } from 'class-validator';
 import { registerDecorator, ValidatorConstraint } from 'class-validator';
 import type { EntitySchema, FindConditions, ObjectType } from 'typeorm';
-import { Connection } from 'typeorm';
+// import { InjectConnection } from '@nestjs/typeorm';
+import { getRepository } from 'typeorm';
+// import { Connection } from 'typeorm';
 
 @Injectable()
 @ValidatorConstraint({ name: 'unique', async: true })
 export class UniqueValidator implements ValidatorConstraintInterface {
-  constructor(@InjectConnection() private readonly connection: Connection) {}
+  //   constructor(@InjectConnection() private readonly connection: Connection) {}
 
   public async validate<E>(
     value: string,
@@ -21,7 +22,8 @@ export class UniqueValidator implements ValidatorConstraintInterface {
     const [entityClass, findCondition = args.property] = args.constraints;
 
     return (
-      (await this.connection.getRepository(entityClass).count({
+      //   (await this.connection.getRepository(entityClass).count({
+      (await getRepository(entityClass).count({
         where:
           typeof findCondition === 'function'
             ? findCondition(args)
